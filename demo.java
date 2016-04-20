58dcaf7ad7d4c8bce206281b0390929369533c15
fuxingyue <7498585@qq.com>
2016-04-20 22:45:16 +0800

    初始化提交

---
+++ demo.java
@@ -0,0 +1,18 @@
+public class demo3{
+	public static void main(String args[]){
+		demo3 d1=new demo3();
+		d1.avg(94,81);
+		
+	}
+
+
+	public void avg(int a,int b){
+		int avgs;
+		avgs=a+b;
+		
+		System.out.println("Êä³ö"+avgs);
+
+		
+
+	}
+}
